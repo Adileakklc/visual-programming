6eceace6edb9ff3513cea7492668f56628b2aabd
Adile Akkılıç <93308733+Adileakklc@users.noreply.github.com>
2024-09-04 23:30:56 +0300

    Add files via upload

---
+++ DosyaIslemleri/Form1.Designer.cs
@@ -0,0 +1,418 @@
+﻿namespace DosyaIslemleri
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.BtnKonumSec = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.BtnOpenFileDialog = new System.Windows.Forms.Button();
+            this.BtnSaveFileDialog = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.TxtYol = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BtnKonum = new System.Windows.Forms.Button();
+            this.BtnOlustur = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtAd = new System.Windows.Forms.TextBox();
+            this.BtnOku = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.BtnYazdir = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TxtKullaniciAd = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.TxtSifre = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.TxtKod1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.TxtKod2 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // BtnKonumSec
+            // 
+            this.BtnKonumSec.Location = new System.Drawing.Point(632, 299);
+            this.BtnKonumSec.Name = "BtnKonumSec";
+            this.BtnKonumSec.Size = new System.Drawing.Size(225, 30);
+            this.BtnKonumSec.TabIndex = 0;
+            this.BtnKonumSec.Text = "Folder Browser Dialog";
+            this.BtnKonumSec.UseVisualStyleBackColor = true;
+            this.BtnKonumSec.Click += new System.EventHandler(this.BtnKonumSec_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(658, 474);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // BtnOpenFileDialog
+            // 
+            this.BtnOpenFileDialog.Location = new System.Drawing.Point(863, 294);
+            this.BtnOpenFileDialog.Name = "BtnOpenFileDialog";
+            this.BtnOpenFileDialog.Size = new System.Drawing.Size(225, 30);
+            this.BtnOpenFileDialog.TabIndex = 3;
+            this.BtnOpenFileDialog.Text = "Open File Dialog";
+            this.BtnOpenFileDialog.UseVisualStyleBackColor = true;
+            this.BtnOpenFileDialog.Click += new System.EventHandler(this.BtnOpenFileDialog_Click);
+            // 
+            // BtnSaveFileDialog
+            // 
+            this.BtnSaveFileDialog.Location = new System.Drawing.Point(863, 330);
+            this.BtnSaveFileDialog.Name = "BtnSaveFileDialog";
+            this.BtnSaveFileDialog.Size = new System.Drawing.Size(225, 30);
+            this.BtnSaveFileDialog.TabIndex = 4;
+            this.BtnSaveFileDialog.Text = "Save File Dialog";
+            this.BtnSaveFileDialog.UseVisualStyleBackColor = true;
+            this.BtnSaveFileDialog.Click += new System.EventHandler(this.BtnSaveFileDialog_Click);
+            // 
+            // TxtYol
+            // 
+            this.TxtYol.Location = new System.Drawing.Point(694, 335);
+            this.TxtYol.Name = "TxtYol";
+            this.TxtYol.Size = new System.Drawing.Size(163, 27);
+            this.TxtYol.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(589, 340);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Belge Yolu";
+            // 
+            // BtnKonum
+            // 
+            this.BtnKonum.Location = new System.Drawing.Point(863, 366);
+            this.BtnKonum.Name = "BtnKonum";
+            this.BtnKonum.Size = new System.Drawing.Size(225, 30);
+            this.BtnKonum.TabIndex = 7;
+            this.BtnKonum.Text = "Konum Seç";
+            this.BtnKonum.UseVisualStyleBackColor = true;
+            this.BtnKonum.Click += new System.EventHandler(this.BtnKonum_Click);
+            // 
+            // BtnOlustur
+            // 
+            this.BtnOlustur.Location = new System.Drawing.Point(863, 402);
+            this.BtnOlustur.Name = "BtnOlustur";
+            this.BtnOlustur.Size = new System.Drawing.Size(225, 30);
+            this.BtnOlustur.TabIndex = 8;
+            this.BtnOlustur.Text = "Oluştur";
+            this.BtnOlustur.UseVisualStyleBackColor = true;
+            this.BtnOlustur.Click += new System.EventHandler(this.BtnOlustur_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(598, 376);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(90, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Belge Adı";
+            // 
+            // TxtAd
+            // 
+            this.TxtAd.Location = new System.Drawing.Point(694, 373);
+            this.TxtAd.Name = "TxtAd";
+            this.TxtAd.Size = new System.Drawing.Size(163, 27);
+            this.TxtAd.TabIndex = 9;
+            // 
+            // BtnOku
+            // 
+            this.BtnOku.Location = new System.Drawing.Point(863, 438);
+            this.BtnOku.Name = "BtnOku";
+            this.BtnOku.Size = new System.Drawing.Size(225, 30);
+            this.BtnOku.TabIndex = 11;
+            this.BtnOku.Text = "Oku";
+            this.BtnOku.UseVisualStyleBackColor = true;
+            this.BtnOku.Click += new System.EventHandler(this.BtnOku_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(647, 406);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(210, 44);
+            this.listBox1.TabIndex = 12;
+            // 
+            // BtnYazdir
+            // 
+            this.BtnYazdir.Location = new System.Drawing.Point(863, 474);
+            this.BtnYazdir.Name = "BtnYazdir";
+            this.BtnYazdir.Size = new System.Drawing.Size(225, 30);
+            this.BtnYazdir.TabIndex = 13;
+            this.BtnYazdir.Text = "Yazdir";
+            this.BtnYazdir.UseVisualStyleBackColor = true;
+            this.BtnYazdir.Click += new System.EventHandler(this.BtnYazdir_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(736, 456);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(121, 54);
+            this.richTextBox1.TabIndex = 14;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(632, 515);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(222, 35);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Form 2\'ye git";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(863, 515);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(222, 35);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Form 3\'e git";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(139, 20);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Kullanıcı Adınız";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // TxtKullaniciAd
+            // 
+            this.TxtKullaniciAd.Location = new System.Drawing.Point(153, 152);
+            this.TxtKullaniciAd.Name = "TxtKullaniciAd";
+            this.TxtKullaniciAd.Size = new System.Drawing.Size(242, 27);
+            this.TxtKullaniciAd.TabIndex = 1;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(153, 314);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(132, 24);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Beni Hatırla";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(593, 78);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 20;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(153, 270);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(98, 38);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Giriş Yap";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // TxtSifre
+            // 
+            this.TxtSifre.Location = new System.Drawing.Point(153, 188);
+            this.TxtSifre.Name = "TxtSifre";
+            this.TxtSifre.Size = new System.Drawing.Size(242, 27);
+            this.TxtSifre.TabIndex = 2;
+            this.TxtSifre.UseSystemPasswordChar = true;
+            this.TxtSifre.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(73, 191);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 20);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "Şifreniz";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.LinkColor = System.Drawing.Color.Black;
+            this.linkLabel1.Location = new System.Drawing.Point(267, 279);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(145, 20);
+            this.linkLabel1.TabIndex = 5;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Şifremi Unuttum";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Algerian", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Maroon;
+            this.label6.Location = new System.Drawing.Point(137, 78);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(275, 31);
+            this.label6.TabIndex = 25;
+            this.label6.Text = "Admin Login Form";
+            // 
+            // TxtKod1
+            // 
+            this.TxtKod1.Location = new System.Drawing.Point(153, 223);
+            this.TxtKod1.Name = "TxtKod1";
+            this.TxtKod1.Size = new System.Drawing.Size(115, 27);
+            this.TxtKod1.TabIndex = 3;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(53, 230);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(94, 20);
+            this.label7.TabIndex = 26;
+            this.label7.Text = "Kodu girin";
+            // 
+            // TxtKod2
+            // 
+            this.TxtKod2.Location = new System.Drawing.Point(280, 223);
+            this.TxtKod2.Name = "TxtKod2";
+            this.TxtKod2.Size = new System.Drawing.Size(115, 27);
+            this.TxtKod2.TabIndex = 28;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 600;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1100, 562);
+            this.Controls.Add(this.TxtKod2);
+            this.Controls.Add(this.TxtKod1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.TxtSifre);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.TxtKullaniciAd);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.BtnYazdir);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.BtnOku);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TxtAd);
+            this.Controls.Add(this.BtnOlustur);
+            this.Controls.Add(this.BtnKonum);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtYol);
+            this.Controls.Add(this.BtnSaveFileDialog);
+            this.Controls.Add(this.BtnOpenFileDialog);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnKonumSec);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button BtnKonumSec;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button BtnOpenFileDialog;
+        private System.Windows.Forms.Button BtnSaveFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox TxtYol;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BtnKonum;
+        private System.Windows.Forms.Button BtnOlustur;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtAd;
+        private System.Windows.Forms.Button BtnOku;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button BtnYazdir;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TxtKullaniciAd;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox TxtSifre;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox TxtKod1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox TxtKod2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ DosyaIslemleri/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DosyaIslemleri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnKonumSec_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            label1.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void BtnOpenFileDialog_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            label1.Text=openFileDialog1.FileName;
+        }
+
+        private void BtnSaveFileDialog_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+
+        }
+        string belgeyolu, belgeadi;
+
+        private void BtnOlustur_Click(object sender, EventArgs e)
+        {
+            belgeadi = TxtAd.Text;
+            StreamWriter sw = File.CreateText(belgeyolu + "\\" + belgeadi + ".txt");
+            MessageBox.Show("Belgeniz başarıyla oluşturuldu", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void BtnOku_Click(object sender, EventArgs e)
+        {
+            if(openFileDialog1.ShowDialog()==DialogResult.OK)
+            {
+                StreamReader oku= new StreamReader(openFileDialog1.FileName);
+                string satir = oku.ReadLine();
+                while(satir != null)
+                {
+                    listBox1.Items.Add(satir);
+                    satir = oku.ReadLine();
+                }
+            }
+        }
+
+        private void BtnYazdir_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.Filter = "Metin Dosyası |*.txt";
+            saveFileDialog1.ShowDialog();
+            StreamWriter kaydet = new StreamWriter(saveFileDialog1.FileName);
+            kaydet.WriteLine(richTextBox1.Text);
+            kaydet.Close();
+            MessageBox.Show("Metin Belgesine kayıt yapıldı", "Bilgi", MessageBoxButtons.OK,MessageBoxIcon.Warning);
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Form2 frm = new Form2();
+            //frm.Show();
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form3 frm = new Form3();
+            frm.Show();
+            this.Hide();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        void kodolustur()
+        {
+            Random rst = new Random();
+            int sayi = rst.Next(10000, 100000);
+            TxtKod2.Text = sayi.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            kodolustur();
+            timer1.Start();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (TxtKullaniciAd.Text == "admin" && TxtSifre.Text == "1234" && TxtKod1.Text == TxtKod2.Text)
+            {
+                Form frm = new Form2
+                {
+                    kullanici = TxtKullaniciAd.Text
+                };
+                frm.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Hatalı bilgi girişi:Kullanıcı adı, şifre veya kod yanlış girildi!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Form3 frm = new Form3();
+            frm.Show();
+        }
+        int sayac = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sayac++;
+            if (sayac %2 ==0)
+            {
+                label6.BackColor = Color.Pink;
+            }
+            else
+            {
+                label6.BackColor = Color.Gold;
+            }
+            if (sayac == 10)
+            {
+                sayac = 0;
+            }
+        }
+
+        private void BtnKonum_Click(object sender, EventArgs e)
+        {
+            if(folderBrowserDialog1.ShowDialog()==DialogResult.OK)
+            {
+                belgeyolu = folderBrowserDialog1.SelectedPath;
+                TxtYol.Text = belgeyolu;
+            }
+        }
+    }
+}
